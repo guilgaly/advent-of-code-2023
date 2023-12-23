4542477456041ac557ac64e9162fc1855599e4d4
Guillaume Galy <guilgaly@gmail.com>
2023-12-23 13:38:39 +0100

    WIP

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -1,33 +1,93 @@
-use std::error::Error;
-use std::num::ParseIntError;
+use common::itertools::Itertools;
 
 static INPUT: &str = include_str!("input");
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let values = parse_input(INPUT)?;
+fn main() {
+    let values = parse_input(INPUT);
 
-    let res1 = part_1(&values);
-    println!("Part 1 result: {}", res1);
-
-    let res2 = part_2(&values);
-    println!("Part 2 result: {}", res2);
-
-    Ok(())
+    // let res1 = part_1(&values);
+    // println!("Part 1 result: {}", res1);
+    //
+    // let res2 = part_2(&values);
+    // println!("Part 2 result: {}", res2);
 }
 
-fn part_1(values: &[u64]) -> usize {
-    values.len()
+fn part_1(heat_loss_map: &HeatLossMap) -> usize {
+    let start = Point { x: 0, y: 0 };
+    let target = Point { x: heat_loss_map.width - 1, y: heat_loss_map.height - 1 };
+
+    0
 }
 
-fn part_2(values: &[u64]) -> usize {
-    values.len()
+// fn part_2(values: &[u64]) -> usize {
+//     values.len()
+// }
+
+fn parse_input(input: &str) -> HeatLossMap {
+    let values = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect_vec()
+        })
+        .collect_vec();
+    let height = values.len();
+    let weight = values[0].len();
+    HeatLossMap { height, width: weight, values }
 }
 
-fn parse_input(input: &str) -> Result<Vec<u64>, ParseIntError> {
-    input.split("\n").map(|line| line.parse::<u64>()).collect()
+impl HeatLossMap {
+    fn neighbours(&self, &Point { x, y }: &Point) -> Vec<(Point, usize)> {
+        let mut res = Vec::new();
+        if x > 0 {
+            res.push((Point { x: x - 1, y }, self.values[y][x - 1]));
+        }
+        if x + 1 < self.width {
+            res.push((Point { x: x + 1, y }, self.values[y][x + 1]));
+        }
+        if y > 0 {
+            res.push((Point { x, y: y - 1 }, self.values[y - 1][x]));
+        }
+        if y + 1 < self.height {
+            res.push((Point { x, y: y + 1 }, self.values[y + 1][x]));
+        }
+        res
+    }
+}
+
+#[derive(Hash, PartialEq, Eq, Debug, Copy, Clone)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+struct HeatLossMap {
+    height: usize,
+    width: usize,
+    values: Vec<Vec<usize>>,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    static TEST_INPUT: &str = "2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse_input(TEST_INPUT)), 102);
+    }
 }
--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,33 +1,168 @@
-use std::error::Error;
-use std::num::ParseIntError;
+use common::itertools::Itertools;
+use common::maplit::hashset;
+use sscanf::sscanf;
+use std::collections::HashSet;
 
 static INPUT: &str = include_str!("input");
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let values = parse_input(INPUT)?;
+fn main() {
+    let digs = parse_input(INPUT);
 
-    let res1 = part_1(&values);
+    let res1 = part_1(&digs);
     println!("Part 1 result: {}", res1);
 
-    let res2 = part_2(&values);
+    let res2 = part_2(&digs);
     println!("Part 2 result: {}", res2);
-
-    Ok(())
 }
 
-fn part_1(values: &[u64]) -> usize {
-    values.len()
+fn part_1(digs: &[Dig]) -> usize {
+    let mut curr_point = Point::new(0, 0);
+    let mut border = hashset![curr_point];
+    for dig in digs {
+        for _ in 0..dig.length {
+            curr_point = curr_point.next(&dig.direction);
+            border.insert(curr_point);
+        }
+    }
+    let min_x = border.iter().map(|p| p.x).min().unwrap();
+    let max_x = border.iter().map(|p| p.x).max().unwrap();
+    let min_y = border.iter().map(|p| p.y).min().unwrap();
+    let max_y = border.iter().map(|p| p.y).max().unwrap();
+    let trench = Trench { border, min_x, max_x, min_y, max_y };
+
+    println!("min_x={}, max_x={}", min_x, max_x);
+    println!("min_y={}, max_y={}", min_y, max_y);
+
+    // Scan line by line
+    let mut count = 0;
+    for y in (trench.min_y..=trench.max_y).rev() {
+        let mut is_inside = false;
+        let mut is_on_wall: Option<bool> = None; // Some(true) means the wall was coming from below, Some(false) it was coming from above
+        for x in trench.min_x..=(trench.max_x + 1) {
+            if trench.border_contains(x, y) {
+                print!("1");
+                count += 1;
+                if trench.border_contains(x, y - 1) && trench.border_contains(x, y + 1) {
+                    is_inside = !is_inside;
+                    is_on_wall = None;
+                } else if is_on_wall.is_none() {
+                    if trench.border_contains(x, y - 1) {
+                        is_on_wall = Some(true);
+                    } else {
+                        is_on_wall = Some(false);
+                    }
+                }
+            } else {
+                if let Some(from_below) = is_on_wall {
+                    if from_below != trench.border_contains(x, y - 1) {
+                        is_inside = !is_inside;
+                    }
+                    is_on_wall = None;
+                }
+                if is_inside {
+                    print!("1");
+                    count += 1;
+                } else {
+                    print!("0");
+                }
+            }
+        }
+        println!();
+    }
+
+    count
 }
 
-fn part_2(values: &[u64]) -> usize {
-    values.len()
+fn part_2(digs: &[Dig]) -> usize {
+    0
 }
 
-fn parse_input(input: &str) -> Result<Vec<u64>, ParseIntError> {
-    input.split("\n").map(|line| line.parse::<u64>()).collect()
+fn parse_input(input: &str) -> Vec<Dig> {
+    input
+        .lines()
+        .map(|line| {
+            let (dir_char, length, color) = sscanf!(line, "{char} {i64} (#{String})").unwrap();
+            let direction = match dir_char {
+                'U' => Direction::Up,
+                'D' => Direction::Down,
+                'L' => Direction::Left,
+                'R' => Direction::Right,
+                _ => panic!("Invalid direction {}", dir_char),
+            };
+            Dig { direction, length, color }
+        })
+        .collect_vec()
+}
+
+impl Trench {
+    fn border_contains(&self, x: i64, y: i64) -> bool {
+        self.border.contains(&Point::new(x, y))
+    }
+}
+
+struct Trench {
+    border: HashSet<Point>,
+    min_x: i64,
+    max_x: i64,
+    min_y: i64,
+    max_y: i64,
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Point {
+        Point { x, y }
+    }
+
+    fn next(&self, dir: &Direction) -> Point {
+        match dir {
+            Direction::Up => Point::new(self.x, self.y + 1),
+            Direction::Down => Point::new(self.x, self.y - 1),
+            Direction::Left => Point::new(self.x - 1, self.y),
+            Direction::Right => Point::new(self.x + 1, self.y),
+        }
+    }
+}
+
+#[derive(Hash, PartialEq, Eq, Copy, Clone, Debug)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+struct Dig {
+    direction: Direction,
+    length: i64,
+    color: String,
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    static TEST_INPUT: &str = "R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&parse_input(TEST_INPUT)), 62);
+    }
 }
