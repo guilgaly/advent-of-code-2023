c637f274a67858d898545a71f3901971547a1f63
Guillaume Galy <guilgaly@gmail.com>
2023-12-23 13:38:02 +0100

    WIP

--- day_22/src/main.rs
+++ day_22/src/main.rs
@@ -1,30 +1,67 @@
-use std::error::Error;
-use std::num::ParseIntError;
+use std::collections::HashSet;
+use sscanf::sscanf;
+use common::itertools::Itertools;
 
 static INPUT: &str = include_str!("input");
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let values = parse_input(INPUT)?;
+fn main() {
+    let bricks = parse_input(INPUT);
 
-    let res1 = part_1(&values);
+    let res1 = part_1(&bricks);
     println!("Part 1 result: {}", res1);
 
-    let res2 = part_2(&values);
+    let res2 = part_2(&bricks);
     println!("Part 2 result: {}", res2);
-
-    Ok(())
 }
 
-fn part_1(values: &[u64]) -> usize {
-    values.len()
+fn part_1(init_bricks: &[Brick]) -> usize {
+    let mut falling_bricks = init_bricks.iter().collect_vec();
+    let mut settled_bricks = Vec::new();
+    let mut settled_cubes = HashSet::new();
+
+
+
+    0
 }
 
-fn part_2(values: &[u64]) -> usize {
-    values.len()
+fn part_2(bricks: &[Brick]) -> usize {
+    0
 }
 
-fn parse_input(input: &str) -> Result<Vec<u64>, ParseIntError> {
-    input.split("\n").map(|line| line.parse::<u64>()).collect()
+fn parse_input(input: &str) -> Vec<Brick> {
+    input.lines()
+        .map(|line| {
+            let (x0, y0, z0, x1, y1, z1) = sscanf!(line, "{usize},{usize},{usize}~{usize},{usize},{usize}").unwrap();
+            Brick(
+                Cube { x: x0, y: y0, z: z0},
+                Cube { x: x1, y: y1, z: z1},
+            )
+        })
+        .collect_vec()
+}
+
+impl Brick {
+    fn cubes(&self) -> Vec<Cube> {
+        if self.0.x == self.1.x && self.0.y == self.1.y {
+            (self.0.z.min(self.1.z)..=self.0.z.max(self.1.z)).map(|z| Cube { x: self.0.x, y: self.0.y, z}).collect_vec()
+        } else if self.0.x == self.1.x && self.0.z == self.1.z {
+            (self.0.y.min(self.1.y)..=self.0.y.max(self.1.y)).map(|y| Cube { x: self.0.x, y, z: self.0.z}).collect_vec()
+        } else if self.0.y == self.1.y && self.0.z == self.1.z {
+            (self.0.x.min(self.1.x)..=self.0.x.max(self.1.x)).map(|x| Cube { x, y: self.0.y, z: self.0.z}).collect_vec()
+        } else {
+            panic!("Invalid brick {:?}", self)
+        }
+    }
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+struct Brick(Cube, Cube);
+
+#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
+struct Cube {
+    x: usize,
+    y: usize,
+    z: usize,
 }
 
 #[cfg(test)]
